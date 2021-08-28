145e82d2d8b0500fc22551fa07f5693fc8806a10
Law Meh <lmeh3@huskers.unl.edu>
2021-08-28 18:26:41 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Law Meh and Richelle Ratzburg
+ *         lmeh3@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Law Meh and Richelle Ratzburg\n");
+
+  return 0;
+}
